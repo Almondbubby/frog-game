bb3c5560df54f35859a6511ab5d19086d1deb377
Almondbubby <79113528+Almondbubby@users.noreply.github.com>
2022-11-23 16:26:06 -0500

    attempted to make grappling work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,25 +7,29 @@ public class Player : MonoBehaviour
     public LineRenderer lr;
     public DistanceJoint2D dj;
     public Camera cam;
+    public float moveSpeed = 200.0f;
+    private bool go = false; //whether or not to move closer to mouse (grapple)
     private void Start()
     {
         dj.enabled = false;
     }
     void Update()
     {
+        swing();
         grapple();
     }
 
-    void grapple()
+    void swing()
     {
         Vector2 mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
-        Debug.DrawRay(transform.position, mousePos-(Vector2)transform.position);
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            //check if theres anything in the way
             RaycastHit2D hit = Physics2D.Raycast(transform.position,mousePos-(Vector2)transform.position);
+
+            //check whether mouse is actually on something thats grapplable
             RaycastHit2D check = Physics2D.Raycast(mousePos, Vector2.zero);
             bool good = true;
-            if (hit.collider != null) Debug.Log(hit.collider.gameObject.layer);
             if(hit.collider != null && hit.collider.gameObject.layer != 3)
             {
                 good = false;
@@ -36,6 +40,8 @@ public class Player : MonoBehaviour
                 dj.enabled = true;
                 dj.connectedAnchor = mousePos;
                 lr.enabled = true;
+
+                //draw line
                 lr.SetPosition(0, transform.position);
                 lr.SetPosition(1, mousePos);
             }
@@ -50,7 +56,41 @@ public class Player : MonoBehaviour
 
         if (dj.enabled)
         {
+            //update line
             lr.SetPosition(0, transform.position);
+        }
+    }
+    void grapple()
+    {
+        Vector2 mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePos - (Vector2)transform.position);
+            RaycastHit2D check = Physics2D.Raycast(mousePos, Vector2.zero);
+            bool good = true;
+            if (hit.collider != null && hit.collider.gameObject.layer != 3)
+            {
+                good = false;
+            }
+
+            if (hit.collider != null && hit.collider.gameObject.layer == 3 && good && check.collider != null)
+            {
+                go = true;
+                lr.enabled = true;
+                lr.SetPosition(0, transform.position);
+                lr.SetPosition(1, mousePos);
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            lr.enabled = false;
+            go = false;
+        }
+
+        if (go)
+        {
+            lr.SetPosition(0, transform.position);
+            transform.position = Vector2.MoveTowards(transform.position, mousePos, moveSpeed * Time.deltaTime);
         }
     }
 }
